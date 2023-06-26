869e5ad3f7a04e0ef693622f7ad649bd80fa59d8
Luke Dixon <github@lukedixon.nl>
2023-06-26 12:40:43 +0200

    create files for all pages

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,32 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_application_1/Profile.dart';
+import 'package:flutter_application_1/Rapports.dart';
+import 'Login.dart';
 
-class HomeScreen extends StatelessWidget {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+class HomeScreen extends StatefulWidget {
+  final User user;
 
-  void _checkLoginStatus() {
-    _auth.authStateChanges().listen((User? user) {
-      if (user == null) {
-        // User is not logged in, navigate to the login screen
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => LoginScreen()),
-        );
-      } else {
-        // User is logged in, navigate to the main app screen
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => MainAppScreen()),
-        );
-      }
-    });
-  }
+  HomeScreen(this.user);
 
   @override
-  void initState() {
-    super.initState();
-    _checkLoginStatus();
-  }
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-  // ...
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  final screens = [
+    Center(
+      child: Text(
+        'Home',
+        style: TextStyle(fontSize: 72),
+      ),
+    ),
+    Rapports(),
+    Profile(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[index],
+      bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+              indicatorColor: Colors.blue.shade400,
+              labelTextStyle: MaterialStateProperty.all(TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white))),
+          child: NavigationBar(
+            height: 70,
+            selectedIndex: index,
+            onDestinationSelected: (index) =>
+                setState(() => this.index = index),
+            backgroundColor: const Color.fromARGB(255, 44, 44, 44),
+            destinations: [
+              NavigationDestination(
+                  icon: Icon(Icons.home_filled, color: Colors.white),
+                  label: 'Home'),
+              NavigationDestination(
+                  icon: Icon(Icons.assignment, color: Colors.white),
+                  label: 'Rapports'),
+              NavigationDestination(
+                  icon: Icon(Icons.person, color: Colors.white),
+                  label: 'Account')
+            ],
+          )),
+    );
+  }
 }
