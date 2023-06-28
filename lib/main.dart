1e5d7bdad5ee86a44da80e9b4e332958246d9b56
ThijsStok <102969782+ThijsStok@users.noreply.github.com>
2023-06-28 10:54:52 +0200

    containers en buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,217 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/cat_fact_api.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final CatApi api = CatApi();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cat Data App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Cat Data'),
-        ),
-        body: FutureBuilder<List<CatData>>(
-          future: api.getAllData(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final catDataList = snapshot.data!;
-              return ListView.builder(
-                itemCount: catDataList.length,
-                itemBuilder: (context, index) {
-                  final catData = catDataList[index];
-                  return ListTile(
-                    title: Text(snapshot.data![index].breed),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CatDataScreen(
-                            breed: snapshot.data![index].breed,
-                            country: snapshot.data![index].country,
-                            coat: snapshot.data![index].coat,
-                            origin: snapshot.data![index].origin,
-                            pattern: snapshot.data![index].pattern,
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              );
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            }
-            return CircularProgressIndicator();
-          },
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(),
     );
   }
 }
 
-class CatDataScreen extends StatelessWidget {
-  final String breed;
-  final String country;
-  final String coat;
-  final String origin;
-  final String pattern;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  CatDataScreen({
-    required this.breed,
-    required this.country,
-    required this.coat,
-    required this.origin,
-    required this.pattern,
-  });
+class _MyHomePageState extends State<MyHomePage> {
+  int _biertjesCount = 0;
+  int _sigarettenCount = 0;
+
+  void _addBiertje(int count) {
+    setState(() {
+      _biertjesCount += count;
+    });
+  }
+
+  void _addSigaret(int count) {
+    setState(() {
+      _sigarettenCount += count;
+    });
+  }
+
+  void _showAddDialog(String title, Function addFunction) {
+    int _count = 1;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return AlertDialog(
+              title: Text(title),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_count > 1) {
+                              _count--;
+                            }
+                          });
+                        },
+                        icon: Icon(Icons.remove),
+                      ),
+                      Text(
+                        '$_count',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _count++;
+                          });
+                        },
+                        icon: Icon(Icons.add),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      addFunction(_count);
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Add'),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cat Details'),
+        title: Text('Flutter Demo'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Breed: $breed'),
-            Text('Country: $country'),
-            Text('Coat: $coat'),
-            Text('Origin: $origin'),
-            Text('Pattern: $pattern'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 200,
+            color: Colors.red,
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Container(
+                      height: 100,
+                      color: Colors.blue,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'biertjes',
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '$_biertjesCount',
+                            style: TextStyle(fontSize: 30),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'sigaretten',
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '$_sigarettenCount',
+                            style: TextStyle(fontSize: 30),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Container(
+        width: double.infinity,
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(50),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: FloatingActionButton(
+                onPressed: () {
+                  _showAddDialog('Add biertje', _addBiertje);
+                },
+                tooltip: 'Add biertje',
+                child: Text(
+                  'biertje',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: FloatingActionButton(
+                onPressed: () {
+                  _showAddDialog('Add sigaret', _addSigaret);
+                },
+                tooltip: 'Add sigaret',
+                child: Text(
+                  'sigaret',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
