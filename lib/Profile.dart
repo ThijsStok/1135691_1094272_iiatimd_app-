538d144c4f7488e99e5f1c93f57e8ae6926864ad
Luke Dixon <github@lukedixon.nl>
2023-06-28 11:39:24 +0200

    basic profile made

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class Profile extends StatefulWidget {
-  const Profile({super.key});
+  const Profile({Key? key});
 
   @override
   State<Profile> createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  User? _currentUser;
+  Map<String, dynamic>? _userData;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentUser = _auth.currentUser;
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    if (_currentUser != null) {
+      DocumentSnapshot snapshot =
+          await _firestore.collection('users').doc(_currentUser!.uid).get();
+      setState(() {
+        _userData = snapshot.data() as Map<String, dynamic>?;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Profile',
-          style: TextStyle(fontSize: 72),
-        ),
-      ),
+      body: _currentUser != null
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('First Name: ${_userData?['firstName'] ?? 'N/A'}'),
+                  Text('Last Name: ${_userData?['lastName'] ?? 'N/A'}'),
+                  Text('Email: ${_userData?['email'] ?? 'N/A'}'),
+                  // Add more Text widgets to display other user fields
+                ],
+              ),
+            )
+          : const CircularProgressIndicator(),
     );
   }
 }
