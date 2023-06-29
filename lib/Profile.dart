75f9e65eef5ef1b3b3ac8bcdac9cabc833b98cf2
ThijsStok <102969782+ThijsStok@users.noreply.github.com>
2023-06-29 13:59:03 +0200

    profile page start

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,20 +1,74 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Profile extends StatefulWidget {
-  const Profile({super.key});
+class Profile extends StatelessWidget {
+  const Profile({Key? key});
 
-  @override
-  State<Profile> createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Profile',
-          style: TextStyle(fontSize: 72),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('profiles').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text('Loading...');
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+              return ProfileCard(
+                email: data['email'],
+                firstName: data['firstName'],
+                lastName: data['lastName'],
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  final String email;
+  final String firstName;
+  final String lastName;
+
+  const ProfileCard({
+    Key? key,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '$firstName $lastName',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              email,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
         ),
       ),
     );
